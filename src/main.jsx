c58996ce6639abc938ae07e0a4931530e1b60bdc
kisnak21 <krisnastya21@gmail.com>
2026-07-21 09:51:02 +0700

    fix: wrap App with AdminProvider in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import './index.css'
 import App from './App.jsx'
 import { ThemeProvider } from './context/ThemeContext.jsx'
+import { AdminProvider } from './context/AdminContext.jsx'
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -22,7 +23,9 @@ createRoot(document.getElementById('root')).render(
       <ThemeProvider>
         <QueryClientProvider client={queryClient}>
           <BrowserRouter>
-            <App />
+            <AdminProvider>
+              <App />
+            </AdminProvider>
           </BrowserRouter>
         </QueryClientProvider>
       </ThemeProvider>
